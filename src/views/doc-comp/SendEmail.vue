cdd5ab08d946a78692272b926dc6cf33a46534e4
ukusatech <ukusatech@gmail.com>
2021-09-11 21:17:24 -0300

    AUTO COMMIT

--- src/views/doc-comp/SendEmail.vue
+++ src/views/doc-comp/SendEmail.vue
@@ -11,19 +11,19 @@
 </template>
 
 <script>
-import emailjs from 'emailjs-com';
+// import emailjs from 'emailjs-com';
 
 export default {
   methods: {
-    sendEmail: (e) => {
-        service_ra7epj1
-      emailjs.sendForm('service_ra7epj1', 'template_z0kedl8', e.target, 'user_NS6sUcl9SiBRXFDIgh0ax')
-        .then((result) => {
-            console.log('SUCCESS!', result.status, result.text);
-        }, (error) => {
-            console.log('FAILED...', error);
-        });
-    }
+    // sendEmail: (e) => {
+    //     service_ra7epj1
+    //   emailjs.sendForm('service_ra7epj1', 'template_z0kedl8', e.target, 'user_NS6sUcl9SiBRXFDIgh0ax')
+    //     .then((result) => {
+    //         console.log('SUCCESS!', result.status, result.text);
+    //     }, (error) => {
+    //         console.log('FAILED...', error);
+    //     });
+    // }
   }
 }
 </script>
